b2ecf411c02bfe3ac4ff2bd94e6b7de2aff89a09
sasumi <cnsasumi@gmail.com>
2022-12-12 10:55:14 +0800

    upd

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ import { showContextMenu } from "./context.js";
 let html = '';
 let asideHtml = '<ul>';
 for(let cat in EmojiList){
-	html += `<h2><a name="${encodeURI(cat)}">${cat}</a></h2><ul class="char-list">`;
-	asideHtml += `<li><a href="#${encodeURI(cat)}">${cat}</a></li>`;
+	html += `<h2><a smooth-anchor="1" href="#${encodeURI(cat)}" name="${encodeURI(cat)}">${cat}</a></h2><ul class="char-list">`;
+	asideHtml += `<li><a smooth-anchor="1" href="#${encodeURI(cat)}">${cat}</a></li>`;
 	EmojiList[cat].forEach(char => {
 		html += `<li data-char="${char}">
 						<span class="char-item" title="click to copy">${char}</span>
@@ -25,11 +25,12 @@ if(document.location.hash){
 	scrollToAnchor(location.hash.substring(1));
 }
 
-document.querySelectorAll('aside ul a').forEach(a => {
+document.querySelectorAll('a[smooth-anchor]').forEach(a => {
 	if(a.hash[0] === '#'){
 		a.addEventListener('click', e=>{
 			scrollToAnchor(a.hash.substring(1));
 			e.preventDefault();
+			history.pushState({}, '', a.hash);
 			return false;
 		});
 	}
